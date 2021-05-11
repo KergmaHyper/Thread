bf5eb8fbd73a22e1c5e44b48f1915a7f3affd4c6
KergmaHyper <kergma@gmail.com>
2021-05-11 13:41:20 +0300

    JThread first start

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.lang.model.element.Name;
 import java.lang.*;
 
 public class Main {
@@ -5,12 +6,27 @@ public class Main {
     public static void main(String[] args) {
       Thread t = Thread.currentThread();
         System.out.println(t.getName());
-      t.setName("ThreadApplication");
+      t.setName("MAIN_ThreadApplication");
         System.out.println(t.getName());
-        System.out.println("wait 3000 miliseconds");
-      //t.wait(3000) ;
+        //System.out.println("wait 3000 miliseconds");
+              System.out.println(t);
 
-        System.out.println(t);
+         JThread jt = new JThread("JounTreadMain");
+         jt.start();
+        System.out.println("ended work Thread - "+Thread.currentThread().getName());
+    }
+}
 
+class JThread extends Thread {
+    JThread(String Name){   super(Name);  }
+
+    public void run(){
+        System.out.println("Started thread - "+Thread.currentThread().getName());
+        try {
+            System.out.println("begin sleep 1000 ms");
+            Thread.sleep(200);
+            System.out.println("end sleep 1000 ms");
+        }catch (InterruptedException ie){System.out.println(ie.getMessage());};
+        System.out.println("ended work Thread - "+Thread.currentThread().getName());
     }
 }
