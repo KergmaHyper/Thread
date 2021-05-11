ff7e1fc1f93848de88d23ced92aeba6ab99e75dd
KergmaHyper <kergma@gmail.com>
2021-05-12 00:01:40 +0300

    interrupt v1 noRunnable

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Runnable rt1 = ()->{
+       /* Runnable rt1 = ()->{
             int i=0;
             System.out.println(">>>>>>>>>>>>>>>>> Start +++"+Thread.currentThread().getName());
             while( (!Thread.currentThread().isInterrupted()&i<10)) {
@@ -22,11 +22,11 @@ public class Main {
                 }
             }
             System.out.println("<<<<<<<<<<<<<<<<< Stop RT1 ---"+Thread.currentThread().getName());
-        };
-         Thread myThr = new Thread(rt1,"MyThread");
+        }; */
+         mThread myThr = new mThread("My_TEST_Thread");
          myThr.start();
          try {
-             Thread.sleep(2000);
+             Thread.sleep(46);
              myThr.interrupt();
          }catch (InterruptedException ie){System.out.println("Interrupt sleep");}
 
@@ -40,3 +40,16 @@ public class Main {
     }
 }
 
+class mThread extends Thread{
+    mThread(String name){super(name);}
+
+    public void run(){
+        System.out.println("Start thread: "+getName());
+        int count=0;
+        while (!isInterrupted()){
+
+            System.out.println("Loop from: "+getName()+" count: "+ count++ + " Interrupt: "+isInterrupted());
+        }
+        System.out.println("Stop thread: "+getName());
+    }
+}
