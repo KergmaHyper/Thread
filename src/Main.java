4cecaf9f6238407f9fa22549df1a69b97e26d306
KergmaHyper <kergma@gmail.com>
2021-05-11 12:50:25 +0300

    start codding

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.lang.*;
+
 public class Main {
 
     public static void main(String[] args) {
+      Thread t = Thread.currentThread();
+        System.out.println(t.getName());
+      t.setName("ThreadApplication");
+        System.out.println(t.getName());
+        System.out.println("wait 3000 miliseconds");
+      //t.wait(3000) ;
 
+        System.out.println(t);
 
     }
 }
