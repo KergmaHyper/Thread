8e19bbcc571fb72f99b4b45a0b9d2fd2aa519621
KergmaHyper <kergma@gmail.com>
2021-05-11 18:00:53 +0300

    use Runnable  Predicate lambda

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 import javax.lang.model.element.Name;
 import javax.sound.midi.Track;
 import java.lang.*;
+import java.util.function.Predicate;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Runnable rt1 = ()->{
-            System.out.println(">>>>>>>>>>>>>>>>> Start RT1 +++"+Thread.currentThread().getName());
+            System.out.println(">>>>>>>>>>>>>>>>> Start ODD RT1 +++"+Thread.currentThread().getName());
             try{ Thread.sleep(1000); }catch (InterruptedException ie){System.out.println("Thread interrupted");}
             System.out.println("<<<<<<<<<<<<<<<<< Stop RT1 ---"+Thread.currentThread().getName());
         };
         Runnable rt2 = ()->{
-            System.out.println("``````````````````` Start RT2 +++"+Thread.currentThread().getName());
+            System.out.println("``````````````````` Start NOT ODD RT2 +++"+Thread.currentThread().getName());
             try{ Thread.sleep(1500); }catch (InterruptedException ie){System.out.println("Thread interrupted");}
             System.out.println("''''''''''''''''''' Stop RT2 ---"+Thread.currentThread().getName());
         };
-        //Thread tr1 = new Thread(rt,"tr1");
-        //tr1.start();
+        Predicate<Integer> odd = (x)->(x%2)==0;
 
         for (int i = 1; i<41;i++){
-            Thread t = new Thread(  ((i%2)==0?rt1:rt2),"Jtread:"+i);
+            Thread t = new Thread(  (odd.test(i)?rt1:rt2),"Jtread:"+i);
             t.start();
             try{
                 t.join(200);
